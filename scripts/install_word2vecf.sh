15e4654bfa89d1d51977726e76e84705bde37824
omerlevy <devnull@localhost>
2015-02-28 21:49:04 +0200

    new version of word2vecf

--- scripts/install_word2vecf.sh
+++ scripts/install_word2vecf.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 mkdir word2vecf
-wget https://bitbucket.org/yoavgo/word2vecf/get/90e299816bcd.zip
-unzip 90e299816bcd.zip
-rm 90e299816bcd.zip
+wget https://bitbucket.org/yoavgo/word2vecf/get/1b94252a58d4.zip
+unzip 1b94252a58d4.zip
+rm 1b94252a58d4.zip
 mv yoavgo-word2vecf-90e299816bcd/*.c word2vecf/.
 mv yoavgo-word2vecf-90e299816bcd/*.h word2vecf/.
 mv yoavgo-word2vecf-90e299816bcd/makefile word2vecf/.
